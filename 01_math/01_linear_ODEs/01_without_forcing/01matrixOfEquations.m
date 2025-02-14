6ce4a998d699876a753bcbeb9162b3df0364319e
asitkumar47 <asit.kumar.1310@gmail.com>
2025-02-14 08:34:02 +0530

    linearODE - create folder structure and blank script of the branch

---
+++ 01_math/01_linear_ODEs/01_without_forcing/01matrixOfEquations.m
@@ -0,0 +1,10 @@
+%% Description
+% this is a simple mass-spring-damper system
+% this is a second order linear ODE
+% this is broken down to a set of first order ODEs
+% and solved using expm (matrix exponential)
+% Intent is to document how to use matlab commands
+% Intent is not to derive equatios
+
+%% Code
+clear, clc, close all
