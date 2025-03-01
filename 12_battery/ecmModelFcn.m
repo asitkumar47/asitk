998bbfe438affa1acb46ffbdafebda3a1187c8b8
asitkumar47 <asit.kumar.1310@gmail.com>
2025-03-01 14:43:31 +0530

    battery - pulse test script update

--- 12_battery/ecmModelFcn.m
+++ 12_battery/ecmModelFcn.m
@@ -21,7 +21,7 @@ function [x, y, debug] = ecmModelFcn(p, xp, u, dt_s)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 1. p    : param struct            (r, c, ocv LUTs)
 % 2. xp   : previous state struct   (Vc1_V, Vc2_V, soc_nd)
-% 3. u    : input struct            (i_A) discharge is positive
+% 3. u    : input struct            (iPack_A) discharge is positive
 % 4. dt_s : time step
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                       Outputs
@@ -35,22 +35,24 @@ function [x, y, debug] = ecmModelFcn(p, xp, u, dt_s)
 % output = ecmModelFcn();
 
 %%
-% find params
-debug.E0_V      = interp1(p.bkptsSoc_nd, p.dataOcv_V,  xp.soc_nd);
-debug.r0_Ohm    = interp1(p.bkptsSoc_nd, p.dataR0_Ohm, xp.soc_nd);
-debug.r1_Ohm    = interp1(p.bkptsSoc_nd, p.dataR1_Ohm, xp.soc_nd);
-debug.c1_F      = interp1(p.bkptsSoc_nd, p.dataC1_Ohm, xp.soc_nd);
+% find params (pack level)
+debug.E0_V      = interp1(p.cell.bkptsSoc_nd, p.cell.dataOcv_V,  xp.soc_nd) * p.pack.nS_nd;
+debug.r0_Ohm    = interp1(p.cell.bkptsSoc_nd, p.cell.dataR0_Ohm, xp.soc_nd) * p.pack.nS_nd / p.pack.nP_nd;
+debug.r1_Ohm    = interp1(p.cell.bkptsSoc_nd, p.cell.dataR1_Ohm, xp.soc_nd) * p.pack.nS_nd / p.pack.nP_nd;
+debug.c1_F      = interp1(p.cell.bkptsSoc_nd, p.cell.dataC1_Ohm, xp.soc_nd) * p.pack.nP_nd / p.pack.nS_nd;
+debug.r2_Ohm    = interp1(p.cell.bkptsSoc_nd, p.cell.dataR2_Ohm, xp.soc_nd) * p.pack.nS_nd / p.pack.nP_nd;
+debug.c2_F      = interp1(p.cell.bkptsSoc_nd, p.cell.dataC2_Ohm, xp.soc_nd) * p.pack.nP_nd / p.pack.nS_nd;
 
 % find state (FE integration)
-x.Vc1_V     = xp.Vc1_V  + dt_s * ( u.i_A / debug.c1_F - xp.Vc1_V / (debug.r1_Ohm * debug.c1_F) );
-x.Vc2_V     = xp.Vc2_V  + dt_s * ( u.i_A / debug.c2_F - xp.Vc2_V / (debug.r2_Ohm * debug.c2_F) );
-x.soc_nd    = xp.soc_nd + dt_s * ( -u.i_A / (p.capacity_Ah/3600) );
+x.Vc1_V     = xp.Vc1_V  + dt_s * ( u.iPack_A / debug.c1_F - xp.Vc1_V / (debug.r1_Ohm * debug.c1_F) );
+x.Vc2_V     = xp.Vc2_V  + dt_s * ( u.iPack_A / debug.c2_F - xp.Vc2_V / (debug.r2_Ohm * debug.c2_F) );
+x.soc_nd    = xp.soc_nd + dt_s * ( -u.iPack_A / (p.cell.capacity_Ah * p.pack.nP_nd / 3600) );
 
 % find output
-y.Vt_V      = debug.E0_V - x.Vc1_V - x.Vc2_V - u.i_A * debug.r0_Ohm;
+y.Vt_V      = debug.E0_V - x.Vc1_V - x.Vc2_V - u.iPack_A * debug.r0_Ohm;
 
 % debug signals
-debug.heatGen_W = (x.i_A^2 * debug.r0_Ohm) + (x.Vc1_V^2 / debug.r1_Ohm) + (x.Vc2_V^2 / debug.r2_Ohm);
+debug.heatGen_W = (x.iPack_A^2 * debug.r0_Ohm) + (x.Vc1_V^2 / debug.r1_Ohm) + (x.Vc2_V^2 / debug.r2_Ohm);
 
 end
 
--- 12_battery/generateTestData.m
+++ 12_battery/generateTestData.m
@@ -4,16 +4,27 @@ clear, clc
 param = getCellPackParamFcn("isShowPlot", false);
 
 % create input for pulse test
-test.cRate_nd   = 5;
-test.restTime_s = 500;
-test.numPulse_nd = 10;
-test.time_s = (0: 1 : param.cell.capacity_Ah * 3600 * param.pack.nP_nd / test.cRate_nd + ...
-                         test.restTime_s * test.numPulse_nd)';
+test.cRateCell_nd   = 5;
+test.numPulse_nd    = 10;
+test.pulseTime_s    = param.cell.capacity_Ah * 3600 / test.cRateCell_nd / test.numPulse_nd;
+test.restTime_s     = 500;
 
-test.currentDraw_A = 0 * test.time_s;
+test.dt_s = 1;
 
+test.time_s = (0: test.dt_s : test.numPulse_nd * (test.pulseTime_s + test.restTime_s))';
+
+packPulseCurrent_A = test.cRateCell_nd * param.pack.nP_nd;
+packCurrentProfile_A = [ones(1, test.pulseTime_s / test.dt_s) * packPulseCurrent_A, ...
+                        zeros(1, test.restTime_s / test.dt_s)]';
+test.packCurrentDraw_A = [repmat(packCurrentProfile_A, test.numPulse_nd, 1); 1];
+clear packPulseCurrent_A packCurrentProfile_A
 
 % perform pulse test
+for iT = 1:length(test.time_s)
+    testData.
+end
 
 
 % simulate data recording (add noise)
+
+% plot simulated test data
