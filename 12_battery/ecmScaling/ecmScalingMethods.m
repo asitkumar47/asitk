17b297d8729054face6bd5a4f2790181a1431cb5
asitkumar47 <asit.kumar.1310@gmail.com>
2025-04-19 22:18:19 -0700

    ecm scaling - sim upload

--- 12_battery/ecmScaling/ecmScalingMethods.m
+++ 12_battery/ecmScaling/ecmScalingMethods.m
@@ -12,30 +12,33 @@
 %   These brnach currents are then passed on to the capacitor voltage ODEs.
 %   This part is same for both methods
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% The example here is 
+% The example here is two ECM unit models in parallel. Each ECM model has
+% two RC pairs
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear, clc
 
 % params (constant)
 ocvA_V = 4; % brnach A OCV
 ocvB_V = 4;
-r0A_Ohm = 0.01;
+r0A_Ohm = 0.005;
 r0B_Ohm = 0.01;
-r1A_Ohm = 0.005;
-r1B_Ohm = 0.005;
+r1A_Ohm = 0.004;
+r1B_Ohm = 0.004;
 r2A_Ohm = 0.007;
-r2B_Ohm = 0.007;
+r2B_Ohm = 0.01;
 c1A_F   = 5 / r1A_Ohm;
 c1B_F   = 5 / r1B_Ohm;
 c2A_F   = 25 / r2A_Ohm;
 c2B_F   = 25 / r2B_Ohm;
 
-% input
-i_A = 10;
-
 % sim
-dt_s = 0.1;
+dt_s = .1;
 t_s = 1:dt_s:100;
+
+% input
+i_A = 10 * ones(1, length(t_s));
+i_A(round(length(i_A)/2) : end) = 0;
 
 % ic
 vc1AM1_V(1) = 0;
@@ -48,28 +51,44 @@ vc1BM2_V(1) = 0;
 vc2AM2_V(1) = 0;
 vc2BM2_V(1) = 0;
 
+
 % sim
 for iT = 1:length(t_s)
     % brnach currents
         % method 1
-        iAM1_A(iT) = (ocvA_V - ocvB_V + i_A * r0B_Ohm - vc1AM1_V(iT) - vc2AM1_V(iT) + vc1BM1_V(iT) + vc2BM1_V(iT)) / (r0A_Ohm + r0B_Ohm);
-        iBM1_A(iT) = (ocvB_V - ocvA_V + i_A * r0A_Ohm - vc1BM1_V(iT) - vc2BM1_V(iT) + vc1AM1_V(iT) + vc2AM1_V(iT)) / (r0A_Ohm + r0B_Ohm);    
+        iAM1_A(iT + 1) = (ocvA_V - ocvB_V + i_A(iT) * r0B_Ohm - vc1AM1_V(iT) - vc2AM1_V(iT) + vc1BM1_V(iT) + vc2BM1_V(iT)) / (r0A_Ohm + r0B_Ohm);
+        iBM1_A(iT + 1) = (ocvB_V - ocvA_V + i_A(iT) * r0A_Ohm - vc1BM1_V(iT) - vc2BM1_V(iT) + vc1AM1_V(iT) + vc2AM1_V(iT)) / (r0A_Ohm + r0B_Ohm);    
         
         % method 2
-        iaM2_A(iT) = r0B_Ohm / (r0A_Ohm + r0B_Ohm);
-        ibM2_A(iT) = r0A_Ohm / (r0A_Ohm + r0B_Ohm);
+        iAM2_A(iT + 1) = i_A(iT) * r0B_Ohm / (r0A_Ohm + r0B_Ohm);
+        iBM2_A(iT + 1) = i_A(iT) * r0A_Ohm / (r0A_Ohm + r0B_Ohm);
 
-    % capacitor voltages
-    vc1AM1_V(iT + 1) = iAM1_A(iT) / c1A_F - vc1AM1_V(iT) / (r1A_Ohm * c1A_F);
-    vc2AM1_V(iT + 1) = iAM1_A(iT) / c2A_F - vc2AM1_V(iT) / (r2A_Ohm * c2A_F);
-    vc1BM1_V(iT + 1) = iBM1_A(iT) / c1B_F - vc1BM1_V(iT) / (r1B_Ohm * c1B_F);
-    vc2BM1_V(iT + 1) = iBM1_A(iT) / c2B_F - vc2BM1_V(iT) / (r2B_Ohm * c2B_F);
+    % capacitor voltages (Forward Euler integration)
+    vc1AM1_V(iT + 1) = vc1AM1_V(iT) + dt_s * (iAM1_A(iT + 1) / c1A_F - vc1AM1_V(iT) / (r1A_Ohm * c1A_F));
+    vc2AM1_V(iT + 1) = vc2AM1_V(iT) + dt_s * (iAM1_A(iT + 1) / c2A_F - vc2AM1_V(iT) / (r2A_Ohm * c2A_F));
+    vc1BM1_V(iT + 1) = vc1BM1_V(iT) + dt_s * (iBM1_A(iT + 1) / c1B_F - vc1BM1_V(iT) / (r1B_Ohm * c1B_F));
+    vc2BM1_V(iT + 1) = vc2BM1_V(iT) + dt_s * (iBM1_A(iT + 1) / c2B_F - vc2BM1_V(iT) / (r2B_Ohm * c2B_F));
 
-    vc1AM2_V(iT + 1) = iAM2_A / c1A_F - vc1AM2_V(iT) / (r1A_Ohm * c1A_F);
-    vc2AM2_V(iT + 1) = iAM2_A / c2A_F - vc2AM2_V(iT) / (r2A_Ohm * c2A_F);
-    vc1BM2_V(iT + 1) = iBM2_A / c1B_F - vc1BM2_V(iT) / (r1B_Ohm * c1B_F);
-    vc2BM2_V(iT + 1) = iBM2_A / c2B_F - vc2BM2_V(iT) / (r2B_Ohm * c2B_F);
+    vc1AM2_V(iT + 1) = vc1AM2_V(iT) + dt_s * (iAM2_A(iT + 1) / c1A_F - vc1AM2_V(iT) / (r1A_Ohm * c1A_F));
+    vc2AM2_V(iT + 1) = vc2AM2_V(iT) + dt_s * (iAM2_A(iT + 1) / c2A_F - vc2AM2_V(iT) / (r2A_Ohm * c2A_F));
+    vc1BM2_V(iT + 1) = vc1BM2_V(iT) + dt_s * (iBM2_A(iT + 1) / c1B_F - vc1BM2_V(iT) / (r1B_Ohm * c1B_F));
+    vc2BM2_V(iT + 1) = vc2BM2_V(iT) + dt_s * (iBM2_A(iT + 1) / c2B_F - vc2BM2_V(iT) / (r2B_Ohm * c2B_F));
 
-    VtM1_V(iT) = ocvA_V - iAM1_A(iT)*r0A_Ohm - vc1AM1_V(iT + 1)
-
+    VtM1_V(iT + 1) = ocvA_V - iAM1_A(iT) * r0A_Ohm - vc1AM1_V(iT + 1) - vc1BM1_V(iT + 1);
+    VtM2_V(iT + 1) = ocvA_V - iAM2_A(iT) * r0A_Ohm - vc1AM2_V(iT + 1) - vc1BM2_V(iT + 1);
 end
+
+figure(1); clf; 
+ax1 = subplot(2, 1, 1);
+yyaxis left
+plot(t_s, VtM1_V(2: end), 'LineWidth', 1); grid on, hold on;
+plot(t_s, VtM2_V(2: end), '--', 'LineWidth', 2); title('Terminal voltage')
+yyaxis right
+plot(t_s, (VtM1_V(2: end) - VtM2_V(2: end)) ./ VtM1_V(2:end) * 100, 'LineWidth', 1);
+legend('Method 1 - simultaneous solving', 'Method 2 - resistor divider', 'error (%)')
+
+ax2 = subplot(2, 1, 2);
+yyaxis left
+plot(t_s, iAM1_A(2:end), 'LineWidth', 1); grid on; hold on;
+plot(t_s, iAM2_A(2:end), '--', 'LineWidth', 2); title('Branch A current')
+legend('Method 1 - simultaneous solving', 'Method 2 - resistor divider')
